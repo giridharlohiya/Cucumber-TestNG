b2a6284193b9878f0dde74758f02513e18476769
Giridhar Lohiya <giridharlohiya22@gmail.com>
2025-06-06 22:35:47 +0530

    Update LoginPage.java

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -5,42 +5,37 @@ import org.openqa.selenium.WebDriver;
 
 import Factory.DriverFactory;
 
-	public class LoginPage 
-	{
-	
+public class LoginPage 
+{
 	private WebDriver driver;
 	DriverFactory driverFactory = new DriverFactory();
-
 	//Constructor
 	public LoginPage(WebDriver driver) {
-        this.driver = DriverFactory.getDriver();
-    }
-		
-	//Locators
-	By Email_ID = By.id("user-name");
-    By Password_ID = By.id("password");
-    By LoginButton_ID = By.id("login-button");
-    By ProductsTextOnHomePage = By.cssSelector(".title");
+	this.driver = DriverFactory.getDriver();
+}		
+//Locators
+By Email_ID = By.id("user-name");
+By Password_ID = By.id("password");
+By LoginButton_ID = By.id("login-button");
+By ProductsTextOnHomePage = By.cssSelector(".title");
     
-    //Methods
-    
-    public boolean Enter_URL() {
-    	driver.get("https://www.saucedemo.com");
-    	boolean EmailVisible = driver.findElement(Email_ID).isDisplayed();
-    	if(EmailVisible){
+//Methods
+public boolean Enter_URL() {
+	driver.get("https://www.saucedemo.com");
+	boolean EmailVisible = driver.findElement(Email_ID).isDisplayed();
+	if(EmailVisible){
 		System.out.println("URL Launch Success");
-    	return true;
+	return true;
 		}
-    	else
-    	System.out.println("URL Launch Fail");
-    	return false;	
-    }
+	else
+	System.out.println("URL Launch Fail");
+	return false;	
+}
     
-    public boolean User_Login() {
-    	driver.findElement(Email_ID).sendKeys("standard_user");
-    	driver.findElement(Password_ID).sendKeys("secret_sauce");
-    	driver.findElement(LoginButton_ID).click();
-    	return driver.findElement(ProductsTextOnHomePage).isDisplayed();
-    }
-	
+public boolean User_Login() {
+	driver.findElement(Email_ID).sendKeys("standard_user");
+	driver.findElement(Password_ID).sendKeys("secret_sauce");
+	driver.findElement(LoginButton_ID).click();
+	return driver.findElement(ProductsTextOnHomePage).isDisplayed();
+}
 }
